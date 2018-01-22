9464dbadc5c3f2b104e38f8047c20d4d4c7630f2
rifkiii <33884039+rifkiii@users.noreply.github.com>
2018-01-22 10:28:29 +0800

    UPDATE ARRAY EXERCISE 3

--- Array-Exercise-3.cpp
+++ Array-Exercise-3.cpp
@@ -1,5 +1,11 @@
+//rifki redha bin ahmad rifae
+//A17DW2273
 //dapatkan 10 input dan keluarkan hasil tambahnya.
 //hasilkan program anda dengan menggunakan array
+#include<iostream>
+using namespace std;
+int main()
+{
 
 
 int array[10];
@@ -10,12 +16,11 @@ cout<<"Enter the 10 numbers:"<<endl;
 for(x=0;x<10;x++)
 {
 
-}
-
 cin>>array[x];
-
 sum=sum+array[x];
-
+}
 cout<<"the sum of the 10 numbers : "<<sum<<endl;
 
 std::cin.get();
+
+}
